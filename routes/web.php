cb9db83429871d9f990a117eafb345fb51e5ea56
brendavrossetto <104913779+brendavrossetto@users.noreply.github.com>
2024-06-06 09:18:52 -0300

    0606

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//criar nossa rota
+Route:: get('/hello', function(){
+    return view('hello');
+});
+
+Route:: get('/hello/{nome}', function($nome)
+{
+    return "ola {$nome} ";
+}); 
+
