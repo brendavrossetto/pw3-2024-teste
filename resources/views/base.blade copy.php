0a8b4db642df146d96f048551ae4eb62be69fd71
brendavrossetto <104913779+brendavrossetto@users.noreply.github.com>
2024-07-19 11:54:47 -0300

    1907 layout tailwind

---
+++ resources/views/base.blade copy.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <tittle>@yield('titulo')</tittle>
+</head>
+<body>
+   
+    <h1>@yield('titulo')</h1>
+    <hr>
+    <a href="{{route('index')}}">Inicial</a>
+    |
+    <a href="{{route('animais')}}">Animais</a>
+    <hr>
+     @if (Auth::user() && Auth::user() ['admin'])
+    <a href="{{route('usuarios')}}">Usuarios</a>
+    <hr>
+    @endif
+    @if (Auth::user())
+    ola, <strong> {{Auth::user()['name']}}</strong>
+    <a href="{{route('logout')}}">Logout</a>
+    @else 
+    <a href="{{route('login')}}">Login</a>
+    @endif
+    <hr>
+    
+    @yield('conteudo')
+</body>
+</html>
