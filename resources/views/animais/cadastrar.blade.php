f8455e71b7e3c2cc0de031e54193f1084f04d556
brendavrossetto <104913779+brendavrossetto@users.noreply.github.com>
2024-09-03 08:56:46 -0300

    imagem sei la
    n sei se deu certo

--- resources/views/animais/cadastrar.blade.php
+++ resources/views/animais/cadastrar.blade.php
@@ -16,7 +16,7 @@
 
 <div class="leading-loose">
  
-<form method="post" action="{{ route ('animais.gravar') }}" class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
+<form method="post" enctype="multipart/form-data" action="{{ route ('animais.gravar') }}" class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
     @csrf
     <input type="text" name="nome" 
     placeholder="Nome" value="{{ old('nome')}}" >
@@ -24,14 +24,8 @@
     <input type="number" name="idade"
     placeholder="Idade" value="{{ old('idade')}}">
     <br>
-    <input type="text" name="diagnostico"
-    placeholder="Diagnostico" value="{{ old('diagnostico')}}">
-    <br>
-    <input type="text" name="dataInternacao"
-    placeholder="Data da internacao" value="{{ old('dataInternacao')}}">
-    <br>
-    <input type="text" name="dataAlta"
-    placeholder="Data da alta" value="{{ old('dataAlta')}}">
+    <input type="file" name="imagem" 
+    placeholder="Imagem" value="{{ old('imagem')}}" >
     <br>
     <input type="submit" name="Gravar">
 </form>
--- resources/views/animais/index.blade.php
+++ resources/views/animais/index.blade.php
@@ -17,9 +17,7 @@
     <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Id</th>
     <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Nome</th>
     <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Idade</th>
-    <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Diagnostico</th>
-    <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Data de Internação</td>
-    <th class="text-left py-3 px-4 uppercase font-semibold text-sm">Data de Alta</td>
+  
     </tr>
     </thead>
     </div>
@@ -31,9 +29,7 @@
 <td class="w-1/3 text-left py-3 px-4">{{ $animal['id'] }}</td>
 <td class="w-1/3 text-left py-3 px-4">{{ $animal['nome'] }}</td>
 <td class="w-1/3 text-left py-3 px-4"> {{ $animal['idade'] }} </td>
-<td class="w-1/3 text-left py-3 px-4"> {{ $animal['diagnostico'] }} </td>
-<td class="w-1/3 text-left py-3 px-4"> {{ $animal['dataInternacao'] }} </td>
-<td class="w-1/3 text-left py-3 px-4"> {{ $animal['dataAlta'] }} </td>
+
 
 {{-- <td><a href="{{route('animal.apagar', $animal['id'])}}">Apagar</a></td> --}}
 </tr>
